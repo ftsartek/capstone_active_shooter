758f5d7310279973eff44f2a2de7013ef3a86f8c
iwachten <isaacachten@gmail.com>
2022-08-14 14:57:47 +1200

    Stop when within line of sight

--- Assets/Scripts/ShooterAI.cs
+++ Assets/Scripts/ShooterAI.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.HID;
+using static UnityEngine.Physics;
 
 public class ShooterAI : MonoBehaviour
 {
@@ -12,15 +14,30 @@ public class ShooterAI : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject playerBody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        agent.SetDestination(player.transform.position);
+    [SerializeField]
+    private Vector3 rayOffset = new Vector3(0, 30, 0);
+
+    [SerializeField]
+    private float maxShootingDistance = Mathf.Infinity;
+
+    void FixedUpdate() {
+        Vector3 startPoint = transform.position + rayOffset;
+        Vector3 rayDirection = (player.transform.position + rayOffset) - startPoint;
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(startPoint, rayDirection, out hit, maxShootingDistance);
+
+        if (hasHit && hit.collider.gameObject.name == playerBody.name) {
+            agent.isStopped = true;
+            Debug.DrawRay(startPoint, rayDirection * hit.distance, Color.green);
+        }
+        else {
+            agent.isStopped = false;
+            agent.SetDestination(player.transform.position);
+            Debug.DrawRay(startPoint, rayDirection * hit.distance, Color.yellow);
+        }
+
     }
 }
