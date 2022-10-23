e116690ce3a8552597606303ad9a277223951d59
Jordan Russell <jordanbrussell@gmail.com>
2022-10-23 13:41:56 +1300

    Cleanup & Sample setup

---
+++ Assets/Required/Scripts/Health.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+  public float maxHealth;
+  [HideInInspector]
+  public float currentHealth;
+  Ragdoll ragdoll;
+  EndMenuScript endMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+      GameObject endFrame = GameObject.Find("EndFrame");
+        endMenu = endFrame.GetComponent<EndMenuScript>();
+        ragdoll = GetComponent<Ragdoll>();
+        currentHealth = maxHealth;
+
+        var rigidBodies = GetComponentsInChildren<Rigidbody>();
+        foreach(var rigidBody in rigidBodies) {
+          HitBox hitBox = rigidBody.gameObject.AddComponent<HitBox>();
+          hitBox.health = this;
+          if (hitBox.gameObject != gameObject) {
+            hitBox.gameObject.layer = LayerMask.NameToLayer("Hitbox");
+          }
+        }
+    }
+
+    public void TakeDamage(float amount, Vector3 direction) {
+      currentHealth -= amount;
+      if (currentHealth <= 0.0f) {
+        Die();
+      }
+    }
+
+    private void Die() {
+      ragdoll.ActivateRagDoll();
+      
+      endMenu.ToggleEndMenu(true, "");
+
+    }
+
+}
